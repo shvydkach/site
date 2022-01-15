0746277eb1cf2a7891b5dcce48c5b49c8e747acd
Швидкач <shvydkach@protonmail.com>
2022-01-15 14:50:17 +0200

    Сторінка 404

--- scripts/categories-modal.js
+++ scripts/categories-modal.js
@@ -1,32 +1,18 @@
-// const openModalButtons = document.querySelectorAll('[data-modal-target]')
+// categories-modal.js: логіка для відкриття-закриття вікна додаткових категорій (modal)
+
+
 const openModalButton = document.querySelector(".categories-mobile-modal-btn span")
 
-// const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const closeModalButton = document.querySelector(".categories-mobile-modal-close")
 
-// const overlay = document.getElementById('overlay')
 const overlay = document.querySelector(".categories-mobile-overlay")
 
 const modal = document.querySelector(".categories-mobile-modal")
 
-// openModalButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const modal = document.querySelector(button.dataset.modalTarget)
-//     openModal(modal)
-//   })
-// })
 openModalButton.addEventListener("click", () => {
   openModal(modal)
 })
 
-
-
-// closeModalButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const modal = button.closest('.modal')
-//     closeModal(modal)
-//   })
-// })
 closeModalButton.addEventListener("click", () => {
   closeModal(modal)
 })
---
+++ scripts/categories-state.js
@@ -0,0 +1,5 @@
+// categories-state.js: керування станами активних категорій новин 
+// (яка саме вибрана), збереження через локальне сховище
+
+localStorage.getItem("category-active")
+localStorage.getItem("category-mobile-add")
