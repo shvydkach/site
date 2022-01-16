cd0d4f62ef6bf7296de80ccc1851059b45f46b78
Швидкач <shvydkach@protonmail.com>
2022-01-16 16:49:35 +0200

    Стилі і розмітка прочитання

--- scripts/fetcher.js
+++ scripts/fetcher.js
@@ -7,13 +7,58 @@ import getDataScience from "./fetch-science.js"
 import getDataCulture from "./fetch-culture.js"
 
 
-// getDataUkraine()
-//   .then(news => console.log(news[1].title))
 
 const newsContainer = document.querySelector(".news")
-
 const categories = document.querySelectorAll(".category")
 
+// Під час першого відкриття, завантажити новини відповідної категорії
+newsContainer.innerHTML = ``
+switch (localStorage.getItem("category-active")) {
+  case "Україна":
+    getDataUkraine()
+      .then(dataUkraine => loadContent(dataUkraine))
+      .catch(err => console.log(err))
+    break;
+  case "Світ":
+    getDataWorld()
+      .then(dataWorld => loadContent(dataWorld))
+      .catch(err => console.log(err))
+    break;
+  case "Уряд":
+    getDataCabinet()
+      .then(dataCabinet => loadContent(dataCabinet))
+      .catch(err => console.log(err))
+    break;
+  case "Спорт":
+    getDataSport()
+      .then(dataSport => loadContent(dataSport))
+      .catch(err => console.log(err))
+    break;
+  case "Економіка":
+    getDataEconomics()
+      .then(dataEconomics => loadContent(dataEconomics))
+      .catch(err => console.log(err))
+    break;
+  case "Наука":
+    getDataScience()
+      .then(dataScience => loadContent(dataScience))
+      .catch(err => console.log(err))
+    break;
+  case "Культура":
+    getDataCulture()
+      .then(dataCulture => loadContent(dataCulture))
+      .catch(err => console.log(err))
+    break;
+  case "Різне":
+    console.log("Вибрано різне")
+    break;
+  default:
+    console.log("Якась помилка")
+    break;
+}
+
+
+// Коли вибирається (натискається), якась з категорій
 categories.forEach(categoryBtn => {
   categoryBtn.addEventListener("click", () => {
     switch (categoryBtn.innerHTML) {
@@ -60,9 +105,17 @@ categories.forEach(categoryBtn => {
         break;
     }
   })
-  
 })
 
+
+
+// Це все буде відбуватись асинхронно
+// Тому, трохи тяжко розділити код в інші файли, 
+// бо він синхронний і не "встигає" завантажити контент
+// (точніше він робить це раніше)
+// Треба буде писати логіку всередині цієї функції,
+// бо знову ж таки, loadContent() викликається з обіцянки
+// функцій getData*Category*() і тому є асинхронною
 function loadContent(data) {
   /* data - це масив з новинами [{"title": "...", ....}, {}, {}] */
 
@@ -75,8 +128,39 @@ function loadContent(data) {
     // ПОКРАЩИТИ: mountNovynaEl(novynaEl) -> можна зробити сторінкування
     newsContainer.appendChild(novynaEl)
   })
+
+  // На цьому етапі новини завантажено в структуру HTML
+  // Тому можна додавати функції для прочитання новини
+
+  // Отримання всіх кнопок "Старт прочитання" (трикутник)
+  const startBtns = document.querySelectorAll(".novyna-start")
+
+  // Коли натискається кнопка "Старт прочитання" (трикутник)
+  startBtns.forEach(startBtn => {
+    startBtn.addEventListener("click", () => {
+      startReader(startBtn)
+    })
+  })
   
 }
+
+
+
+
+// Допоміжні функції:
+
+
+function startReader(clickedBtn) {
+  //  Div, до якого належить ця кнопка. Він зберігає дані через dataset
+  const novynaEl = clickedBtn.parentElement
+  const content = novynaEl.dataset.content
+  const links = novynaEl.dataset.links
+
+  // reader.classList.add("active")
+  // localStorage.setItem("reading", true)
+
+}
+
 
 
 function createNovynaElement(novynaDataArr) {
@@ -112,8 +196,3 @@ function createNovynaElement(novynaDataArr) {
   return novynaEl
 }
 
-
-
-// function check() {
-//   const currentActiveCategory = localStorage.getItem("category-active")
-// }
